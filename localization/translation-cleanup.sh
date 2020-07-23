15eab06bff76fcf42b023d6ffe4e6b03484c5092
Michael Smith <smithmichael@salesforce.com>
2020-07-23 08:54:02 -0400

    Update the localization script to remove the JQ dependency

--- localization/translation-cleanup.sh
+++ localization/translation-cleanup.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 
-## Verify that JQ is installed before continuing
-if ! command -v jq &> /dev/null
+## Verify that hasSampleData is set to false before continuing
+if grep -q '"hasSampleData": true' orgs/feature.json
 then
-    echo "JQ is not installed. Use 'brew install jq' to install JQ from the command line."
+    echo "WARNING: The feature.json still has \"hasSampleData\" set to true. Cannot continue until this is set to false."
     exit
 fi
 
-ECHO "# === Create a temporary org definition file with sample data disabled"
-mv orgs/feature.json orgs/feature-save.json
-jq '.hasSampleData = false' orgs/feature-save.json > orgs/temp.json
-jq '.orgName = "NPSP - Translations"' orgs/temp.json > orgs/feature.json
+echo "###############################################################################################"
+echo "What this script does:"
+echo "1. Deploy all metadata to a scratch org"
+echo "2. Use the cci 'retrieve_unpackaged' command to retrieve all configuration from the org into the src folder"
+echo "3. Reset and/or delete any retrieved metadata that is unrelalated to translations"
+echo "4. Execute the cci 'cleanup_translation_metadata' task against the metadata to strip invalid or extranous xml content"
+echo ""
+echo ""
+echo "When the script has fully completed, the developer should review any pending changes in an IDE, such as VSCode"
+echo "to verify that all changes are expected."
+echo ""
+echo "ONLY changes to the 'objectTranslation' and 'translation' files should be commited to the repository."
+echo "ALL OTHER CHANGED FILES SHOULD BE IGNORED."
+echo "###############################################################################################"
+read -s -n 1 -p "Press any key to continue ..."
+echo ""
 
-ECHO "# ==== CREATE THE SCRATCH ORG ===="
+echo "# ==== CREATE THE SCRATCH ORG ===="
 cci org remove translations
 cci org scratch feature translations --days 1
 cci flow run dependencies --org translations 
 cci flow run deploy_unmanaged --org translations
 cci task run update_admin_profile --org translations
 
-rm orgs/temp.json
-mv orgs/feature-save.json orgs/feature.json
-
-ECHO "# ==== RESET the Package.xml and use retrieve_unpackaged to pull down the metadata ===="
+echo "# ==== RESET the Package.xml and use retrieve_unpackaged to pull down the metadata ===="
 git checkout src/package.xml
 cci task run retrieve_unpackaged --org translations -o path src -o package_xml src/package.xml
 
 cci org remove translations
 
-ECHO "# ==== DISCARD other files pulled down by the above task that we do not need ===="
+echo "# ==== DISCARD other files pulled down by the above task that we do not need ===="
 git checkout src/classes/*
 git checkout src/homePageComponents/*
 git checkout src/labels/*
@@ -41,7 +50,7 @@ git checkout src/weblinks/*
 git checkout src/workflows/*
 git checkout src/package.xml
 
-ECHO "# ==== CLEANUP task to strip out extraneous elements from the translation files ===="
+echo "# ==== CLEANUP task to strip out extraneous elements from the translation files ===="
 cci task run cleanup_translation_metadata
 
-ECHO "# ==== DONE! - Review the remaining modified translation files before committing back into the branch"
+echo "# ==== DONE! - Review the remaining modified translation files before committing back into the branch"
